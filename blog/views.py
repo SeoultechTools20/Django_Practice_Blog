214f9ad47bb2216426bc73709c95b659e1a5a244
SeoultechTools20 <dlxo444@gmail.com>
2022-02-14 16:40:06 +0900

    22.02.14 update

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.utils import timezone
+
 
 # Create your views here.
+# 글 상세정보
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post_key': post})
+
+
 # Views 내에 선언된 함수로 인자로 HttpRequest 라는 객체를 Django가 전달해준다.
 def post_list(request):
     my_name = '장고웹프레임워크'
@@ -15,7 +23,7 @@ def post_list(request):
     #     <p>Http Path : {mypath}</p>
     # '''.format(name=my_name, method=http_method, header=request.headers['user-agent'], mypath=request.path))
 
-    from django.utils import timezone
+
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'post_list': posts})
 
